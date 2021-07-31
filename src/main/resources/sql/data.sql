2f8714a30bd47814d85da6bf5a62d365e8a1ec7e
chasyujang <86553408+chasyujang@users.noreply.github.com>
2021-07-31 09:52:45 +0900

    data
    data

---
+++ src/main/resources/sql/data.sql
@@ -0,0 +1,21 @@
+INSERT INTO `user` (`id`, `email`, `name`, `tel`) VALUES
+(1, 'kim@naver.com', 'Kim', '010-0000-0001'),
+(2, 'janny@naver.com', 'Janny', '010-0000-0002'),
+(3, 'billie@naver.com', 'Billie Eilish', '010-0000-0003'),
+(4, 'taylor@naver.com', 'Taylor Swift', '010-0000-0004'),
+(5, 'jack@naver.com', 'Jack', '010-0000-0005'),
+(6, 'hong@hanmail.net', '홍길동', '010-0000-0006'),
+(7, 'kang@naver.com', '강감찬', '010-0000-1111');
+INSERT INTO `product` (`no`, `name`) VALUES
+('10001', 'TV'),
+('10002', 'Notebook'),
+('10003', 'Phone'),
+('10004', 'Monitor'),
+('10005', 'Washer'),
+('10006', 'Car'),
+('10007', 'Table'),
+('10008', 'Beer'),
+('10009', 'Bike'),
+('10010', 'Boat');
+
+--출처: https://oingdaddy.tistory.com/264 [SI Supply Depot]
---
+++ src/main/resources/sql/schema.sql
@@ -0,0 +1,12 @@
+DROP TABLE IF EXISTS user;
+CREATE TABLE user(
+id INT PRIMARY KEY,
+email VARCHAR(30),
+name VARCHAR(30),
+tel VARCHAR(30)
+);
+DROP TABLE IF EXISTS product;
+CREATE TABLE product (
+no VARCHAR(5),
+name VARCHAR(50)
+);
