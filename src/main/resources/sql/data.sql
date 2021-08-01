5de010764535e5e14bbe0a3923c4a0a5179e689d
chasyujang <86553408+chasyujang@users.noreply.github.com>
2021-08-01 12:37:23 +0900

    hikari
    error....

--- src/main/resources/sql/data.sql
+++ src/main/resources/sql/data.sql
@@ -1,21 +1,16 @@
-INSERT INTO `user` (`id`, `email`, `name`, `tel`) VALUES
-(1, 'kim@naver.com', 'Kim', '010-0000-0001'),
-(2, 'janny@naver.com', 'Janny', '010-0000-0002'),
-(3, 'billie@naver.com', 'Billie Eilish', '010-0000-0003'),
-(4, 'taylor@naver.com', 'Taylor Swift', '010-0000-0004'),
-(5, 'jack@naver.com', 'Jack', '010-0000-0005'),
-(6, 'hong@hanmail.net', '홍길동', '010-0000-0006'),
-(7, 'kang@naver.com', '강감찬', '010-0000-1111');
-INSERT INTO `product` (`no`, `name`) VALUES
-('10001', 'TV'),
-('10002', 'Notebook'),
-('10003', 'Phone'),
-('10004', 'Monitor'),
-('10005', 'Washer'),
-('10006', 'Car'),
-('10007', 'Table'),
-('10008', 'Beer'),
-('10009', 'Bike'),
-('10010', 'Boat');
--- dd
---출처: https://oingdaddy.tistory.com/264 [SI Supply Depot]
+INSERT INTO `employee` (`code`, `id`, `name`, `department`,`pay`) VALUES
+(A001, 'kim1234', 'Kim', 'SM',3000),
+(A002, 'janny11', 'Janny', 'SM',4000),
+(A003, 'billie55', 'Billie Eilish', 'SM',5000),
+(B001, 'taylor1004', 'Taylor Swift', 'SI',6000),
+(B002, 'jackP', 'Jack', 'SI',4000),
+(C001, 'hong9', '홍길동', 'SI',2000),
+(C002, 'kang1', '강감찬', 'SI',3000);
+INSERT INTO `result` (`code`, `result`,`plus`,`minus`,`absent`) VALUES
+(A001, 5, 30, 10, 0),
+(A002, 4, 40, 20, 0),
+(A003, 5, 70, 10, 0),
+(B001, 7, 50, 20, 1),
+(B002, 6, 30, 10,1),
+(C001, 4, 60, 30,2),
+(C002, 5, 20, 10,0);
